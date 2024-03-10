b7edfe031fd03635eba9627017ce75a50f9dbcef
Mayank Singh <121190587+mayaaannnnk@users.noreply.github.com>
2024-03-10 17:14:26 +0530

    Add files via upload
    Initialize an Array at compile Time

---
+++ Arrays/InitializeAnArrayAtCompileTime.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={1,2,5,6,4};
+    printf("elements of Array are :");
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
